c221453e295bffc83c7131fe09d27a6b0cd53ceb
kdizzlle <kelly.mary.williams@gmail.com>
2025-06-26 17:32:31 -0700

    wip commit for tanya to try gds

--- scales/scalesSvc/JetsonPowerModeManager/JetsonPowerModeManager.cpp
+++ scales/scalesSvc/JetsonPowerModeManager/JetsonPowerModeManager.cpp
@@ -10,6 +10,8 @@
 #include <string>
 #include <iostream>
 
+char get_nvp_mode();
+
 namespace scalesSvc {
 
   // ----------------------------------------------------------------------
@@ -85,25 +87,15 @@ namespace scalesSvc {
     )
   {
     // TODO
-    // std::system("/usr/sbin/nvpmodel -q"); // executes the NVIDIA power mode control command
-    // returns the current power mode
-    FILE *mode_file = popen("/usr/sbin/nvpmodel -q", "r");
+    char modeNow = get_nvp_mode();
+    if (modeNow == 'e')
+    {//error getting power mode from jetson
 
-    char buffer[128];
-    std::string mode_str = "";
-    while (fgets(buffer, sizeof(buffer), mode_file) != NULL)
-    { //read until end of process
-      mode_str+=buffer;
     }
-    if(!mode_str.empty())
-    {
-      mode_str.pop_back(); //delete last character of string
-      mode_str.erase(0,15); //deletes irrelevant output
-
-      this->powerModeSend_out(0, mode_str);
-
-      this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+    else{//return jetson power mode
+      this->
     }
+    
     
   }
 
@@ -115,6 +107,34 @@ namespace scalesSvc {
   {
     // TODO
     this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+  }
+
+  // ----------------------------------------------------------------------
+  // Private functions
+  // ----------------------------------------------------------------------
+
+  char get_nvp_mode()
+  {
+    char nvpMode = 0;
+    std::string mode_str = "";
+
+    FILE *mode_file = popen("/usr/sbin/nvpmodel -q", "r");
+
+    char buffer[128];
+    while (fgets(buffer, sizeof(buffer), mode_file) != NULL)
+    { //read until end of process
+      mode_str+=buffer;
+    }
+    if(!mode_str.empty())
+    {//if mode_str is not empty (we got output)
+      nvpMode = mode_str.back(); //save mode identifier number
+      mode_str.pop_back(); //delete mode number from string
+      mode_str.erase(0,15); //deletes irrelevant output
+      return nvpMode;
+    }
+    else{//mode_str is empty (no output)
+      return 'e';
+    }
   }
 
 }
