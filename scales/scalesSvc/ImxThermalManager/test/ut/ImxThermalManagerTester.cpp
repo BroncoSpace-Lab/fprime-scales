cc93964dc2253954b72cf53831a739efe0175a06
LucaLanzi <luquilanzi@gmail.com>
2026-06-07 01:38:03 -0700

    Created Unit Test Files for ImxThermalManager

---
+++ scales/scalesSvc/ImxThermalManager/test/ut/ImxThermalManagerTester.cpp
@@ -0,0 +1,40 @@
+// ======================================================================
+// \title  ImxThermalManagerTester.cpp
+// \author luquito
+// \brief  cpp file for ImxThermalManager component test harness implementation class
+// ======================================================================
+
+#include "ImxThermalManagerTester.hpp"
+
+namespace scalesSvc {
+
+  // ----------------------------------------------------------------------
+  // Construction and destruction
+  // ----------------------------------------------------------------------
+
+  ImxThermalManagerTester ::
+    ImxThermalManagerTester() :
+      ImxThermalManagerGTestBase("ImxThermalManagerTester", ImxThermalManagerTester::MAX_HISTORY_SIZE),
+      component("ImxThermalManager")
+  {
+    this->initComponents();
+    this->connectPorts();
+  }
+
+  ImxThermalManagerTester ::
+    ~ImxThermalManagerTester()
+  {
+
+  }
+
+  // ----------------------------------------------------------------------
+  // Tests
+  // ----------------------------------------------------------------------
+
+  void ImxThermalManagerTester ::
+    ImxThermalManagerTesting()
+  {
+    
+  }
+
+}
