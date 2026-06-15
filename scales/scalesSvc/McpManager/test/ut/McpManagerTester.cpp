e2677868a3ddc91408dcc5a1a6e42b9dbba0dc19
Dat Nguyen <bidat2007@gmail.com>
2026-06-15 14:06:07 -0700

    Wip of McpManager unit testing

--- scales/scalesSvc/McpManager/test/ut/McpManagerTester.cpp
+++ scales/scalesSvc/McpManager/test/ut/McpManagerTester.cpp
@@ -35,25 +35,51 @@ namespace scalesSvc {
     // Load Parameters
     this->component.loadParameters(); //load the component parameters
 
-    // --- 1st cycle when device just booted --- 
+    // --- when device just booted --- 
     this->invoke_to_run(0, 0); // Trigger the component's run port
     this->component.doDispatch(); // Execute run_handler, write telemetry, and queues the tick 
     this->component.doDispatch(); // Tick is processed, since we're in INIT, doRead gets called and the device just booted so parameters are set up
 
-    printf("The parameters are set up with the following values: \n");
-    printf("IDLE_LOW_THR: %f\n", this->component.IDLE_LOW_THR);
-    printf("IDLE_HIGH_THR: %f\n", this->component.IDLE_HIGH_THR);
-    printf("WARN_LOW_THR: %f\n", this->component.WARN_LOW_THR);
-    printf("WARN_HIGH_THR: %f\n", this-> component.WARN_HIGH_THR);
-    printf("FAULT_LOW_THR: %f\n", this->component.FAULT_LOW_THR);
-    printf("FAULT_HIGH_THR: %f\n", this->component.FAULT_HIGH_THR);
 
-    // --- 2nd cycle, device will started reading sensor values ---
+    // --- device will started reading sensor values ---
     this->invoke_to_run(0, 0); // Trigger the component's run port
     this->component.doDispatch(); // Execute run_handler, write telemetry, and queues the tick 
-    this->component.doDispatch(); // Tick is processed, since we're in INIT, doRead gets called and sensor reading is attempted. 
-    // this->component.doDispatch(); // Finish processing the read 
+    this->component.doDispatch(); // Tick is processed, since we're in INIT, doRead gets called and sensor reading is attempted.
+    // Note: in unit testing environment, the i2c bus always evalute successfully, and the temperature values read are always 0
+    this->component.doDispatch(); // Since the read was successful, success signal is sent, entered EVALUATE state
+
+    
+    // --- device will start evaluating sensor values ---
+
+    // --- First case is check to evaluation for IDLE ---
+    // Set the temperature readings to be within the IDLE threshold values
+    for(int i = 0; i < 3; i++){
+      this->component.m_thermalReadings[i].settemperature((this->component.IDLE_LOW_THR + this->component.IDLE_HIGH_THR) / 2);
+    }
+
+    this->invoke_to_run(0, 0); // Trigger the component's run port
+    this->component.doDispatch(); // Execute run_handler, write telemetry, and queues the tick 
+    this->component.doDispatch(); // Since we're in EVALUATE, doEvaluate gets called 
+
+    /* Check if telmetry was emmited*/
+    ASSERT_TLM_IMX_TEMP_SIZE(1);
+    ASSERT_TLM_PERIPHERAL_TEMP_SIZE(1);
+    ASSERT_TLM_JETSON_TEMP_SIZE(1);
+
+    /* Check if the states are correct */
+    const ThermalReading& imx =
+        this->tlmHistory_IMX_TEMP->at(0).arg;
+    const ThermalReading& peripheral =
+        this->tlmHistory_PERIPHERAL_TEMP->at(0).arg;
+    const ThermalReading& jetson =
+        this->tlmHistory_JETSON_TEMP->at(0).arg;
+
+    ASSERT_EQ(imx.gettempState(), scalesSvc::ThermalStates::IDLE);
+    ASSERT_EQ(peripheral.gettempState(), scalesSvc::ThermalStates::IDLE);
+    ASSERT_EQ(jetson.gettempState(), scalesSvc::ThermalStates::IDLE);
+
+    this->component.doDispatch(); // Finish evaluating and transition back to initial state to read temp again on next tick
     // check if the read is successful
-   // ASSERT_FROM_PORT_HISTORY_SIZE(1);
+    // ASSERT_FROM_PORT_HISTORY_SIZE(1);
   }
 }
